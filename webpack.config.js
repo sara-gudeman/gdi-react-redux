2c4f3705c3fc038eeb723adf675904724a4759a7
Sara Gudeman <sarag@sarag01.local>
2017-07-14 18:25:24 -0700

    gets initial webpack config working

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 9000
+  }
+};
