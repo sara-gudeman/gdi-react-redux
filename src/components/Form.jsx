34b8b17051da420fa75fb6bc557a6451b8c0c8de
Sara Gudeman <sgudeman982@gmail.com>
2017-07-15 21:36:10 -0700

    adds logging to console for form submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,16 +3,17 @@ import React from 'react';
 class Form extends React.Component {
 	constructor(props) {
 		super(props);
-		// this.onSubmit = this.onSubmit.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
 	}
 	onSubmit(e) {
 		e.preventDefault();
 		console.log('submitting')
+		console.log(this.input.value)
 	}
 	render() {
 		return (
 			<form>
-				<input />
+				<input ref={(node) => {this.input = node}} />
 				<button onClick={this.onSubmit}>Submit</button>
 			</form>
 		);
