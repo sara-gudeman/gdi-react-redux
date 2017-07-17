6a2b89f95b9b5b48d48841e859401e384bbae87b
Sara Gudeman <sarag@sarag01.local>
2017-07-17 13:01:37 -0700

    use state instead of refs in form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,17 +5,38 @@ import { uploadImage } from '../redux/actionCreators';
 class Form extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			userInput: ''
+		};
 		this.onSubmit = this.onSubmit.bind(this);
+		this.handleInputChange = this.handleInputChange.bind(this);
 	}
 	onSubmit(e) {
 		e.preventDefault();
 		console.log('submitting')
-		this.props.handleUploadImage(this.input.value);
+		// normally uploading an image would be some kind of promise and we could reset the userInput after success
+		// this is a little messy
+		this.props.handleUploadImage(this.state.userInput);
+		this.setState(() => {
+			return {
+				userInput: ''
+			};
+		});
+	}
+	handleInputChange(e) {
+		// this is a tricky part...if we just user simple setState we don't need to do this
+		const value = e.target.value;
+		// do we want to just do a simple setState here with an object? or keep using functions?
+		this.setState(() => {
+			return {
+				userInput: value
+			};
+		});
 	}
 	render() {
 		return (
 			<form className="Form">
-				<input ref={(node) => {this.input = node}} />
+				<input placeholder="enter a url here" value={this.state.userInput} onChange={this.handleInputChange} />
 				<button onClick={this.onSubmit}>Submit</button>
 			</form>
 		);
