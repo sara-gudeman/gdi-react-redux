38b0a28e4692accc16c1ca8f4c107ee516f54c42
Sara Gudeman <sarag@sarag01.local>
2017-07-17 14:33:59 -0700

    functional components where possible

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-class ProfilePage extends React.Component {
-	render() {
-		return (
-			<div className="ProfilePage">
-				This is a profile page.
-			</div>
-		);
-	}
-}
+const ProfilePage = () => {
+    return (
+        <div className="ProfilePage">
+            This is a profile page.
+        </div>
+    );
+};
 
 export default ProfilePage;
