9b345bb8863f93e1efd57d666b8a6402cd2fa08e
Sara Gudeman <sgudeman982@gmail.com>
2017-07-18 10:23:07 -0700

    adds in extra code samples

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,15 @@
+// create a class component that toggles a button on and off
+import React from 'react';
+
+// make a class component that renders 'Hello World' in a div
+class HelloWorld extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello world
+            </div>
+        );
+    }
+}
+
+export default HelloWorld;
---
+++ src/components/NameAnnouncer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+// we'll do this
+class NameAnnouncer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userInput: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+  handleInputChange(event) {
+    const value = event.target.value;
+    this.setState({
+      userInput: value
+    });
+  }
+  handleFormSubmit(event) {
+    event.preventDefault();
+    alert(this.state.userInput);
+    this.setState({
+      userInput: ''
+    });
+  }
+  render() {
+    return (
+      <form className="NameAnnouncer">
+        <input placeholder="enter your name here" value={this.state.userInput} onChange={this.handleInputChange} />
+        <button onClick={this.handleFormSubmit}>Submit</button>
+      </form>
+    );
+  }
+}
+
+export default NameAnnouncer;
