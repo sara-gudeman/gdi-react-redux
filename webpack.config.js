de41ab16d91357e521e16fa9ec5bf74671835d54
Sara Gudeman <sgudeman982@gmail.com>
2017-07-18 11:32:35 -0700

    add comments to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,27 @@
 var path = require('path');
 
 module.exports = {
+  // entry: webpack's entry file, where webpack will start looking to build it's dependency graph
   entry: './src/index.js',
+  // output: where webpack will put the 'bundle' of code it made from packaging dependencies
   output: {
+    // the filename of the output file
     filename: 'bundle.js',
+    // the path to the directory where the output file should go
     path: path.resolve(__dirname, 'dist')
   },
+  // these are configurations for the dev server
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     historyApiFallback: true,
     port: 9000
   },
+  // webpack only understands javascript! loaders transform files into modules for webpack
   module: {
+    // this is an array of loaders
     loaders: [
+      // test determines which file extensions to run the loader on
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.css$/, use: ['style-loader', 'css-loader']}
