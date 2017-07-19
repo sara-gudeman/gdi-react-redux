c837ef4693543be42923b001069e506bf8424a11
Sara Gudeman <sgudeman982@gmail.com>
2017-07-18 19:02:20 -0700

    add in app component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
-// import your app component here!
+import App from './App.jsx';
 
 ReactDOM.render(
-    // replace this div with your app component!
-    <div>Hello GDInsta!</div>,
+    <App />,
     document.getElementById('app-root')
 );
