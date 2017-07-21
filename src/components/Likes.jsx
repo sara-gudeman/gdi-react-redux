a93c0572ec4bb01d1c57932d6a3751424c6e5127
Sara Gudeman <sgudeman982@gmail.com>
2017-07-21 13:35:45 -0700

    add in more exercises

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -1,30 +1,3 @@
 import React from 'react';
 
-class Likes extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			numLikes: this.props.totalLikes
-		};
-		this.handleLikeButtonClick = this.handleLikeButtonClick.bind(this);
-	}
-	handleLikeButtonClick() {
-		this.setState((prevState) => {
-			return {
-				numLikes: prevState.numLikes + 1
-			};
-		});
-	}
-	render() {
-		return (
-			<div className="Likes">
-				<span>{ this.state.numLikes } likes</span>
-				<span className="Likes-like-button" onClick={this.handleLikeButtonClick}>
-					Like
-				</span>
-			</div>
-		);
-	}
-}
-
-export default Likes;
+// create a likes component here
