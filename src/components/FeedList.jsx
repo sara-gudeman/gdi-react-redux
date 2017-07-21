ef7e9b0f9df9b22f8fc5fda1cb29ac07298f691e
Sara Gudeman <sgudeman982@gmail.com>
2017-07-21 13:32:57 -0700

    move data

--- src/components/FeedList.jsx
+++ src/components/FeedList.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import FeedItem from './FeedItem.jsx';
-import data from '../../data.json';
-
-const { photos } = data;
 
 // create a FeedList component here
 // for each photo, create a FeedItem component
