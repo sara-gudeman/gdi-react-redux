fa3f87706de125f2b183aef9107036ca6c20359b
Sara Gudeman <sgudeman982@gmail.com>
2017-07-21 18:23:31 -0700

    get rid of name announcer solution

--- src/components/NameAnnouncer.jsx
+++ src/components/NameAnnouncer.jsx
@@ -1,36 +1,3 @@
 import React from 'react';
 
 // we'll do this
-class NameAnnouncer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userInput: ''
-    };
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-  }
-  handleInputChange(event) {
-    const value = event.target.value;
-    this.setState({
-      userInput: value
-    });
-  }
-  handleFormSubmit(event) {
-    event.preventDefault();
-    alert(this.state.userInput);
-    this.setState({
-      userInput: ''
-    });
-  }
-  render() {
-    return (
-      <form className="NameAnnouncer">
-        <input placeholder="enter your name here" value={this.state.userInput} onChange={this.handleInputChange} />
-        <button onClick={this.handleFormSubmit}>Submit</button>
-      </form>
-    );
-  }
-}
-
-export default NameAnnouncer;
