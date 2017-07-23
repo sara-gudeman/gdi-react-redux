e274fe250e9eb0659b7fcfe5a0c96e8f892b4f18
coldzonemt <coldzonemt@yahoo.com>
2017-07-23 15:08:39 -0700

    gets rid of unneeded import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import ProfilePage from './pages/ProfilePage.jsx';
 import AboutPage from './pages/AboutPage.jsx';
 import HomePage from './pages/HomePage.jsx';
